543367d6cd1e5f30ebbf8f5f9ce6f6710eeb03fd
Nawaf Khaled <2240007430@iau.edu.sa>
2026-02-05 09:54:28 +0300

    Update LoadDataSet.py

--- Lab2/LoadDataSet.py
+++ Lab2/LoadDataSet.py
@@ -1,6 +1,10 @@
 import pandas as pd
 df = pd.read_csv('US_Covid_Mobility_2021.csv')
-print(f"Dataset Shape: {df.shape}")
+rows, columns = df.shape
+print(f"Number of Rows: {rows:,}")
+print(f"Number of Columns: {columns}")
+print("-" * 30)
+print(f"Dataset Shape (Tuple): {df.shape}")
 print("-" * 30)
 print("First 5 rows:")
 print(df.head())
